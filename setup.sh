b3e354de7417cedc13687a11186bf7e9e997a765
iván <lidoryph@gmail.com>
2025-01-20 13:16:22 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,29 +1,28 @@
 echo THE PASSWORD IS chimera
-apk add git
+doas apk add git
 echo THE PASSWORD IS chimera
-su -
-wipefs -a /dev/sda
-sfdisk /dev/sda <<EOF
+doas wipefs -a /dev/sda
+doas sfdisk /dev/sda <<EOF
 label: gpt
 name=esp, size=100M, type="EFI System"
 name=swap, size=8G, type"Linux Swap"
 name=root
 EOF
-mkfs.vfat /dev/sda1
-mkswap /dev/sda2
-mkfs.ext4 /dev/sda3
-mkdir /media/root
-mount /dev/sda3
-mkdir -p /media/root/boot/efi
-mount /dev/sda1 /media/root/boot/efi
-chmod 755 /media/root
-chimera-bootstrap -l /media/root
+doas mkfs.vfat /dev/sda1
+doas mkswap /dev/sda2
+doas mkfs.ext4 /dev/sda3
+doas mkdir /media/root
+doas mount /dev/sda3
+doas mkdir -p /media/root/boot/efi
+doas mount /dev/sda1 /media/root/boot/efi
+doas chmod 755 /media/root
+doas chimera-bootstrap -l /media/root
 git clone https://github.com/lidoryph/chimera-setup
 cd chimera-setup
-cp chroot-setup.sh /media/root
+doas cp chroot-setup.sh /media/root
 chimera-chroot /media/root
-rm /media/root/chroot-setup.sh
+doas rm /media/root/chroot-setup.sh
 cd ..
-rm -rf chimera-setup
+doas rm -rf chimera-setup
 exit
 reboot
